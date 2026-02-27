8bad5e4c965c1c1a1fb965e31e7871c157b57db2
Mayara <mayara.s.almeida8@aluno.senai.br>
2026-02-27 10:59:27 -0300

    :tada: Scaffold da API criada

---
+++ RoyalGames/RoyalGames/Contexts/RoyalGamesContext.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using RoyalGames.Domains;
+
+namespace RoyalGames.Contexts;
+
+public partial class RoyalGamesContext : DbContext
+{
+    public RoyalGamesContext()
+    {
+    }
+
+    public RoyalGamesContext(DbContextOptions<RoyalGamesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ClassificacaoIndicativa> ClassificacaoIndicativa { get; set; }
+
+    public virtual DbSet<Genero> Genero { get; set; }
+
+    public virtual DbSet<Jogo> Jogo { get; set; }
+
+    public virtual DbSet<Log_AlteracaoJogo> Log_AlteracaoJogo { get; set; }
+
+    public virtual DbSet<Plataforma> Plataforma { get; set; }
+
+    public virtual DbSet<Usuario> Usuario { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=RoyalGames;Trusted_Connection=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ClassificacaoIndicativa>(entity =>
+        {
+            entity.HasKey(e => e.ClassificacaoIndicativaID).HasName("PK__Classifi__892DEC6F562B0CA6");
+
+            entity.HasIndex(e => e.Classificacao, "UQ__Classifi__AE2C4A0FD5B7FDAA").IsUnique();
+
+            entity.Property(e => e.Classificacao)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Genero>(entity =>
+        {
+            entity.HasKey(e => e.GeneroID).HasName("PK__Genero__A99D0268787E3BEF");
+
+            entity.HasIndex(e => e.Nome, "UQ__Genero__7D8FE3B225B0DAF5").IsUnique();
+
+            entity.Property(e => e.Nome)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Jogo>(entity =>
+        {
+            entity.HasKey(e => e.JogoID).HasName("PK__Jogo__5919685582C1DA07");
+
+            entity.ToTable(tb =>
+                {
+                    tb.HasTrigger("trg_AlteracaoJogo");
+                    tb.HasTrigger("trg_ExclusaoJogo");
+                });
+
+            entity.HasIndex(e => e.Nome, "UQ__Jogo__7D8FE3B204664059").IsUnique();
+
+            entity.Property(e => e.Nome)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Preco).HasColumnType("decimal(10, 2)");
+            entity.Property(e => e.StatusJogo).HasDefaultValue(true);
+
+            entity.HasOne(d => d.ClassificacaoIndicativa).WithMany(p => p.Jogo)
+                .HasForeignKey(d => d.ClassificacaoIndicativaID)
+                .HasConstraintName("FK__Jogo__Classifica__534D60F1");
+
+            entity.HasOne(d => d.Usuario).WithMany(p => p.Jogo)
+                .HasForeignKey(d => d.UsuarioID)
+                .HasConstraintName("FK__Jogo__UsuarioID__5441852A");
+
+            entity.HasMany(d => d.Genero).WithMany(p => p.Jogo)
+                .UsingEntity<Dictionary<string, object>>(
+                    "JogoGenero",
+                    r => r.HasOne<Genero>().WithMany()
+                        .HasForeignKey("GeneroID")
+                        .HasConstraintName("FK_JogoGenero_Genero"),
+                    l => l.HasOne<Jogo>().WithMany()
+                        .HasForeignKey("JogoID")
+                        .HasConstraintName("FK_JogoGenero_Jogo"),
+                    j =>
+                    {
+                        j.HasKey("JogoID", "GeneroID");
+                    });
+
+            entity.HasMany(d => d.Plataforma).WithMany(p => p.Jogo)
+                .UsingEntity<Dictionary<string, object>>(
+                    "JogoPlataforma",
+                    r => r.HasOne<Plataforma>().WithMany()
+                        .HasForeignKey("PlataformaID")
+                        .HasConstraintName("FK_JogoPlataforma_Plataforma"),
+                    l => l.HasOne<Jogo>().WithMany()
+                        .HasForeignKey("JogoID")
+                        .HasConstraintName("FK_JogoPlataforma_Jogo"),
+                    j =>
+                    {
+                        j.HasKey("JogoID", "PlataformaID");
+                    });
+        });
+
+        modelBuilder.Entity<Log_AlteracaoJogo>(entity =>
+        {
+            entity.HasKey(e => e.Log_AlteracaJogoID).HasName("PK__Log_Alte__8ABFB3585D58EF8C");
+
+            entity.Property(e => e.DataAlteracao).HasPrecision(0);
+            entity.Property(e => e.NomeAnterior)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Precoanterior).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.Jogo).WithMany(p => p.Log_AlteracaoJogo)
+                .HasForeignKey(d => d.JogoID)
+                .HasConstraintName("FK__Log_Alter__JogoI__6477ECF3");
+        });
+
+        modelBuilder.Entity<Plataforma>(entity =>
+        {
+            entity.HasKey(e => e.PlataformaID).HasName("PK__Platafor__B835678D28B5E548");
+
+            entity.HasIndex(e => e.Nome, "UQ__Platafor__7D8FE3B23532F0E4").IsUnique();
+
+            entity.Property(e => e.Nome)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.UsuarioID).HasName("PK__Usuario__2B3DE79864674D6B");
+
+            entity.ToTable(tb => tb.HasTrigger("trg_ExclusaoUsuario"));
+
+            entity.HasIndex(e => e.Email, "UQ__Usuario__A9D10534B4F7B50F").IsUnique();
+
+            entity.Property(e => e.Email)
+                .HasMaxLength(150)
+                .IsUnicode(false);
+            entity.Property(e => e.Nome)
+                .HasMaxLength(60)
+                .IsUnicode(false);
+            entity.Property(e => e.Senha).HasMaxLength(32);
+            entity.Property(e => e.StatusUsuario).HasDefaultValue(true);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ RoyalGames/RoyalGames/Domains/Log_AlteracaoJogo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RoyalGames.Domains;
+
+public partial class Log_AlteracaoJogo
+{
+    public int Log_AlteracaJogoID { get; set; }
+
+    public DateTime DataAlteracao { get; set; }
+
+    public string? NomeAnterior { get; set; }
+
+    public decimal? Precoanterior { get; set; }
+
+    public int? JogoID { get; set; }
+
+    public virtual Jogo? Jogo { get; set; }
+}
