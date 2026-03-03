ff40640c16bc7f0f1501075fa329a2ece63c2b98
Maria <maria.c.ramos18@aluno.senai.br>
2026-03-03 10:53:05 -0300

    :fire: Remoção de arquivos indesejados

---
+++ RoyalGames/DTOs/AutenticacaoDto/TokenDto.cs
@@ -0,0 +1,8 @@
+﻿namespace RoyalGames.DTOs.AutenticacaoDto
+{
+    public class TokenDto
+    {
+        //criei uma string para o token ode ela tem que receber diferente de nulo 
+        public string Token { get; set; } = null!;
+    }
+}
