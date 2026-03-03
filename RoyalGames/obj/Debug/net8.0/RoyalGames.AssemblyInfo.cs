838f0a338706347895924d33efa0e94f487d2f61
Mayara <mayara.s.almeida8@aluno.senai.br>
2026-03-03 09:47:13 -0300

    :see_no_evil: Adição do Gitignore

--- RoyalGames/obj/Debug/net8.0/RoyalGames.AssemblyInfo.cs
+++ RoyalGames/obj/Debug/net8.0/RoyalGames.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoyalGames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24d2641a30d590226b6bc5882b409cb7bf53feca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ceb642b9c725ffdfd83e87788413397687fc0414")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoyalGames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoyalGames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
