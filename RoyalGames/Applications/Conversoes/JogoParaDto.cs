878f078f459565f9b477293826673678221a0daa
Mayara <mayara.s.almeida8@aluno.senai.br>
2026-03-03 10:51:05 -0300

    :fire: Remoção de arquivos indesejados

---
+++ RoyalGames/Applications/Conversoes/JogoParaDto.cs
@@ -0,0 +1,20 @@
+﻿using RoyalGames.Domains;
+using RoyalGames.DTOs.JogoDto;
+
+namespace RoyalGames.Applications.Conversoes
+{
+    public class JogoParaDto
+    {
+        public static LerJogoDto ConverterParaDto(Jogo jogo)
+        {
+            return new LerJogoDto
+            {
+                JogoID = jogo.JogoID,
+                Nome = jogo.Nome,
+                Preco = jogo.Preco,
+                Descricao = jogo.Descricao,
+                StatusJogo = jogo.StatusJogo
+            };
+        }
+    }
+}
---
+++ RoyalGames/DTOs/JogoDto/AtualizarJogoDto.cs
@@ -0,0 +1,21 @@
+﻿namespace RoyalGames.DTOs.JogoDto
+{
+    public class AtualizarJogoDto
+    {
+        public string Nome { get; set; } = null!;
+
+        public decimal Preco { get; set; }
+
+        public string Descricao { get; set; } = null!;
+
+        public IFormFile Imagem { get; set; } = null!;
+
+        public int? ClassificacaoIndicativaID { get; set; }
+
+        public List<int> GenerosIds { get; set; } = new();
+
+        public List<int> PlataformasIds { get; set; } = new();
+
+        public bool? StatusJogo { get; set; }
+    }
+}
