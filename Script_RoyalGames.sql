8abfe551e5870df3a63003ab0b5cdfa27a44bef6
Mayara <mayara.s.almeida8@aluno.senai.br>
2026-03-05 10:52:07 -0300

    :docs: Script completo do banco de dados

---
+++ Script_RoyalGames.sql
@@ -0,0 +1,351 @@
+USE [master]
+GO
+/****** Object:  Database [RoyalGames]    Script Date: 05/03/2026 10:50:10 ******/
+CREATE DATABASE [RoyalGames]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'RoyalGames', FILENAME = N'C:\Users\47014999869\RoyalGames.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'RoyalGames_log', FILENAME = N'C:\Users\47014999869\RoyalGames_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
+GO
+ALTER DATABASE [RoyalGames] SET COMPATIBILITY_LEVEL = 170
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [RoyalGames].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [RoyalGames] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [RoyalGames] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [RoyalGames] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [RoyalGames] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [RoyalGames] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [RoyalGames] SET AUTO_CLOSE ON 
+GO
+ALTER DATABASE [RoyalGames] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [RoyalGames] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [RoyalGames] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [RoyalGames] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [RoyalGames] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [RoyalGames] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [RoyalGames] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [RoyalGames] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [RoyalGames] SET  ENABLE_BROKER 
+GO
+ALTER DATABASE [RoyalGames] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [RoyalGames] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [RoyalGames] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [RoyalGames] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [RoyalGames] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [RoyalGames] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [RoyalGames] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [RoyalGames] SET RECOVERY SIMPLE 
+GO
+ALTER DATABASE [RoyalGames] SET  MULTI_USER 
+GO
+ALTER DATABASE [RoyalGames] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [RoyalGames] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [RoyalGames] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [RoyalGames] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [RoyalGames] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [RoyalGames] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+ALTER DATABASE [RoyalGames] SET OPTIMIZED_LOCKING = OFF 
+GO
+ALTER DATABASE [RoyalGames] SET QUERY_STORE = ON
+GO
+ALTER DATABASE [RoyalGames] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
+GO
+USE [RoyalGames]
+GO
+/****** Object:  Table [dbo].[ClassificacaoIndicativa]    Script Date: 05/03/2026 10:50:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ClassificacaoIndicativa](
+	[ClassificacaoIndicativaID] [int] IDENTITY(1,1) NOT NULL,
+	[Classificacao] [varchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ClassificacaoIndicativaID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Genero]    Script Date: 05/03/2026 10:50:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Genero](
+	[GeneroID] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[GeneroID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Jogo]    Script Date: 05/03/2026 10:50:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Jogo](
+	[JogoID] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](150) NOT NULL,
+	[Preco] [decimal](10, 2) NOT NULL,
+	[Descricao] [nvarchar](max) NOT NULL,
+	[Imagem] [varbinary](max) NOT NULL,
+	[StatusJogo] [bit] NULL,
+	[ClassificacaoIndicativaID] [int] NULL,
+	[UsuarioID] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[JogoID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[JogoGenero]    Script Date: 05/03/2026 10:50:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[JogoGenero](
+	[JogoID] [int] NOT NULL,
+	[GeneroID] [int] NOT NULL,
+ CONSTRAINT [PK_JogoGenero] PRIMARY KEY CLUSTERED 
+(
+	[JogoID] ASC,
+	[GeneroID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[JogoPlataforma]    Script Date: 05/03/2026 10:50:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[JogoPlataforma](
+	[JogoID] [int] NOT NULL,
+	[PlataformaID] [int] NOT NULL,
+ CONSTRAINT [PK_JogoPlataforma] PRIMARY KEY CLUSTERED 
+(
+	[JogoID] ASC,
+	[PlataformaID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Log_AlteracaoJogo]    Script Date: 05/03/2026 10:50:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Log_AlteracaoJogo](
+	[Log_AlteracaJogoID] [int] IDENTITY(1,1) NOT NULL,
+	[DataAlteracao] [datetime2](0) NOT NULL,
+	[NomeAnterior] [varchar](100) NULL,
+	[Precoanterior] [decimal](10, 2) NULL,
+	[JogoID] [int] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Log_AlteracaJogoID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Plataforma]    Script Date: 05/03/2026 10:50:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Plataforma](
+	[PlataformaID] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](50) NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[PlataformaID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Usuario]    Script Date: 05/03/2026 10:50:10 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Usuario](
+	[UsuarioID] [int] IDENTITY(1,1) NOT NULL,
+	[Nome] [varchar](60) NOT NULL,
+	[Email] [varchar](150) NOT NULL,
+	[Senha] [varbinary](32) NOT NULL,
+	[StatusUsuario] [bit] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[UsuarioID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+SET IDENTITY_INSERT [dbo].[ClassificacaoIndicativa] ON 
+GO
+INSERT [dbo].[ClassificacaoIndicativa] ([ClassificacaoIndicativaID], [Classificacao]) VALUES (2, N'+16 anos')
+GO
+INSERT [dbo].[ClassificacaoIndicativa] ([ClassificacaoIndicativaID], [Classificacao]) VALUES (1, N'+18 anos')
+GO
+INSERT [dbo].[ClassificacaoIndicativa] ([ClassificacaoIndicativaID], [Classificacao]) VALUES (3, N'Livre')
+GO
+SET IDENTITY_INSERT [dbo].[ClassificacaoIndicativa] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Genero] ON 
+GO
+INSERT [dbo].[Genero] ([GeneroID], [Nome]) VALUES (2, N'Ação')
+GO
+INSERT [dbo].[Genero] ([GeneroID], [Nome]) VALUES (1, N'Aventura')
+GO
+INSERT [dbo].[Genero] ([GeneroID], [Nome]) VALUES (3, N'Tiro tático')
+GO
+SET IDENTITY_INSERT [dbo].[Genero] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Jogo] ON 
+GO
+INSERT [dbo].[Jogo] ([JogoID], [Nome], [Preco], [Descricao], [Imagem], [StatusJogo], [ClassificacaoIndicativaID], [UsuarioID]) VALUES (1, N'Minecraft', CAST(29.90 AS Decimal(10, 2)), N'Explore e crie livremente em um mundo totalmente aberto.', 0x696D6167656D20616C6561746F726961, 1, 3, 1)
+GO
+INSERT [dbo].[Jogo] ([JogoID], [Nome], [Preco], [Descricao], [Imagem], [StatusJogo], [ClassificacaoIndicativaID], [UsuarioID]) VALUES (2, N'Red Dead Redemption 2', CAST(120.50 AS Decimal(10, 2)), N'O fim da era do velho oeste se aproxima, e os xerifes caçam as últimas gangues fora da lei.', 0x696D6167656D20616C6561746F726961, 1, 1, 1)
+GO
+INSERT [dbo].[Jogo] ([JogoID], [Nome], [Preco], [Descricao], [Imagem], [StatusJogo], [ClassificacaoIndicativaID], [UsuarioID]) VALUES (3, N'Valorant', CAST(69.90 AS Decimal(10, 2)), N'Personagens marcantes, mecânica de tiro precisa e habilidades únicas!', 0x696D6167656D20616C6561746F726961, 1, 2, 1)
+GO
+SET IDENTITY_INSERT [dbo].[Jogo] OFF
+GO
+INSERT [dbo].[JogoGenero] ([JogoID], [GeneroID]) VALUES (1, 1)
+GO
+INSERT [dbo].[JogoGenero] ([JogoID], [GeneroID]) VALUES (2, 2)
+GO
+INSERT [dbo].[JogoGenero] ([JogoID], [GeneroID]) VALUES (3, 3)
+GO
+INSERT [dbo].[JogoPlataforma] ([JogoID], [PlataformaID]) VALUES (1, 1)
+GO
+INSERT [dbo].[JogoPlataforma] ([JogoID], [PlataformaID]) VALUES (2, 1)
+GO
+INSERT [dbo].[JogoPlataforma] ([JogoID], [PlataformaID]) VALUES (3, 1)
+GO
+SET IDENTITY_INSERT [dbo].[Plataforma] ON 
+GO
+INSERT [dbo].[Plataforma] ([PlataformaID], [Nome]) VALUES (1, N'PlayStation')
+GO
+SET IDENTITY_INSERT [dbo].[Plataforma] OFF
+GO
+SET IDENTITY_INSERT [dbo].[Usuario] ON 
+GO
+INSERT [dbo].[Usuario] ([UsuarioID], [Nome], [Email], [Senha], [StatusUsuario]) VALUES (1, N'Mayara Almeida', N'mayara@royalgames.com', 0x7676AAAFB027C825BD9ABAB78B234070E702752F625B752E55E55B48E607E358, 1)
+GO
+INSERT [dbo].[Usuario] ([UsuarioID], [Nome], [Email], [Senha], [StatusUsuario]) VALUES (2, N'Maria Clara', N'mariaClara@royalgames.com', 0x7676AAAFB027C825BD9ABAB78B234070E702752F625B752E55E55B48E607E358, 1)
+GO
+INSERT [dbo].[Usuario] ([UsuarioID], [Nome], [Email], [Senha], [StatusUsuario]) VALUES (3, N'Rafa Hahon', N'RafaellaHahon@gmail.com', 0x240BE518FABD2724DDB6F04EEB1DA5967448D7E831C08C8FA822809F74C720A9, 0)
+GO
+SET IDENTITY_INSERT [dbo].[Usuario] OFF
+GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Index [UQ__Classifi__AE2C4A0FD5B7FDAA]    Script Date: 05/03/2026 10:50:11 ******/
+ALTER TABLE [dbo].[ClassificacaoIndicativa] ADD UNIQUE NONCLUSTERED 
+(
+	[Classificacao] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Index [UQ__Genero__7D8FE3B225B0DAF5]    Script Date: 05/03/2026 10:50:11 ******/
+ALTER TABLE [dbo].[Genero] ADD UNIQUE NONCLUSTERED 
+(
+	[Nome] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Index [UQ__Jogo__7D8FE3B204664059]    Script Date: 05/03/2026 10:50:11 ******/
+ALTER TABLE [dbo].[Jogo] ADD UNIQUE NONCLUSTERED 
+(
+	[Nome] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Index [UQ__Platafor__7D8FE3B23532F0E4]    Script Date: 05/03/2026 10:50:11 ******/
+ALTER TABLE [dbo].[Plataforma] ADD UNIQUE NONCLUSTERED 
+(
+	[Nome] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+SET ANSI_PADDING ON
+GO
+/****** Object:  Index [UQ__Usuario__A9D10534B4F7B50F]    Script Date: 05/03/2026 10:50:11 ******/
+ALTER TABLE [dbo].[Usuario] ADD UNIQUE NONCLUSTERED 
+(
+	[Email] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[Jogo] ADD  DEFAULT ((1)) FOR [StatusJogo]
+GO
+ALTER TABLE [dbo].[Usuario] ADD  DEFAULT ((1)) FOR [StatusUsuario]
+GO
+ALTER TABLE [dbo].[Jogo]  WITH CHECK ADD FOREIGN KEY([ClassificacaoIndicativaID])
+REFERENCES [dbo].[ClassificacaoIndicativa] ([ClassificacaoIndicativaID])
+GO
+ALTER TABLE [dbo].[Jogo]  WITH CHECK ADD FOREIGN KEY([UsuarioID])
+REFERENCES [dbo].[Usuario] ([UsuarioID])
+GO
+ALTER TABLE [dbo].[JogoGenero]  WITH CHECK ADD  CONSTRAINT [FK_JogoGenero_Genero] FOREIGN KEY([GeneroID])
+REFERENCES [dbo].[Genero] ([GeneroID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[JogoGenero] CHECK CONSTRAINT [FK_JogoGenero_Genero]
+GO
+ALTER TABLE [dbo].[JogoGenero]  WITH CHECK ADD  CONSTRAINT [FK_JogoGenero_Jogo] FOREIGN KEY([JogoID])
+REFERENCES [dbo].[Jogo] ([JogoID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[JogoGenero] CHECK CONSTRAINT [FK_JogoGenero_Jogo]
+GO
+ALTER TABLE [dbo].[JogoPlataforma]  WITH CHECK ADD  CONSTRAINT [FK_JogoPlataforma_Jogo] FOREIGN KEY([JogoID])
+REFERENCES [dbo].[Jogo] ([JogoID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[JogoPlataforma] CHECK CONSTRAINT [FK_JogoPlataforma_Jogo]
+GO
+ALTER TABLE [dbo].[JogoPlataforma]  WITH CHECK ADD  CONSTRAINT [FK_JogoPlataforma_Plataforma] FOREIGN KEY([PlataformaID])
+REFERENCES [dbo].[Plataforma] ([PlataformaID])
+ON DELETE CASCADE
+GO
+ALTER TABLE [dbo].[JogoPlataforma] CHECK CONSTRAINT [FK_JogoPlataforma_Plataforma]
+GO
+ALTER TABLE [dbo].[Log_AlteracaoJogo]  WITH CHECK ADD FOREIGN KEY([JogoID])
+REFERENCES [dbo].[Jogo] ([JogoID])
+GO
+USE [master]
+GO
+ALTER DATABASE [RoyalGames] SET  READ_WRITE 
+GO
