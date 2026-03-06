c4b2d46d9c96b5470e83f1b77ecbd961be112e68
Mayara <mayara.s.almeida8@aluno.senai.br>
2026-03-06 17:21:15 -0300

    :sparkles: Adição do CRUD de Jogo e Autenticação

--- RoyalGames/obj/Debug/net8.0/RoyalGames.AssemblyInfo.cs
+++ RoyalGames/obj/Debug/net8.0/RoyalGames.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RoyalGames")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+646b17b4bfdb5ae130b90e9abfe8f9f2be11a57d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a4f335fe7f0c120708eaa378ceb3ca71dbe517")]
 [assembly: System.Reflection.AssemblyProductAttribute("RoyalGames")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RoyalGames")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
